c02cff5e76415e409b613a39a2b45ece0c1ab962
rahat9016 <minhajurrohoman9016@gmail.com>
2026-09-01 10:15:29 +0600

    Initial commit

---
+++ src/main/java/com/familiemunshi/FamiliemunshiApplication.java
@@ -0,0 +1,13 @@
+package com.familiemunshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliemunshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamiliemunshiApplication.class, args);
+    }
+
+}

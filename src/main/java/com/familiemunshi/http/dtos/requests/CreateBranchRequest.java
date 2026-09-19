ac77c29b70ff7d2cd9b998f2d463453580bc7930
rahat9016 <minhajurrohoman9016@gmail.com>
2026-09-20 00:16:28 +0600

    Add: Create Branch Request body

---
+++ src/main/java/com/familiemunshi/http/dtos/requests/CreateBranchRequest.java
@@ -0,0 +1,20 @@
+package com.familiemunshi.http.dtos.requests;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public class CreateBranchRequest {
+    @Schema(description = "Name of the branch", example = "Mirpur Branch", requiredMode = REQUIRED)
+    @NotBlank(message="Branch name is required")
+    @Min(2)
+    private String name;
+
+    @Schema(description = "Address of the branch", example = "Mirpur-10, Dhaka", requiredMode = NOT_REQUIRED)
+    private String address;
+
+    @Schema(description = "Branch active status", example = "true", defaultValue = "true", requiredMode = NOT_REQUIRED)
+    private Boolean isActive = true;
+
+}
--- src/main/java/com/familiemunshi/mapper/BranchMapper.java
+++ src/main/java/com/familiemunshi/mapper/BranchMapper.java
@@ -1,4 +1,30 @@
 package com.familiemunshi.mapper;
 
+import com.familiemunshi.jpa.daos.BranchDao;
+import com.familiemunshi.service.entities.Branch;
+
 public class BranchMapper {
+    private BranchMapper(){
+        throw new IllegalStateException("Utility class should not be instantiated");
+    }
+
+    public static Branch toEntity(BranchDao dao) {
+        if(dao == null) return null;
+        return Branch.builder()
+                .id(dao.getId())
+                .name(dao.getName())
+                .address(dao.getAddress())
+                .isActive(dao.isActive())
+                .build();
+    }
+
+    public static BranchDao toDao(Branch domain){
+        if(domain == null) return null;
+        return BranchDao.builder()
+                .id(domain.id())
+                .name(domain.name())
+                .address(domain.address())
+                .isActive(domain.isActive())
+                .build();
+    }
 }
